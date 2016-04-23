e89a45ea96a34ab93c8d1aef4e053257f7a0fbce
ThanhNienNghiemTuc <admin@admins-Air.lan>
2016-04-23 13:29:13 +0700

    first commit

---
+++ ChatApp/JSQChatApp.h
@@ -0,0 +1,13 @@
+//
+//  JSQChatApp.h
+//  ChatApp
+//
+//  Created by admin on 4/22/16.
+//  Copyright © 2016 baoTranIOS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSQChatApp : NSObject
+
+@end
---
+++ ChatApp/JSQChatApp.m
@@ -0,0 +1,13 @@
+//
+//  JSQChatApp.m
+//  ChatApp
+//
+//  Created by admin on 4/22/16.
+//  Copyright © 2016 baoTranIOS. All rights reserved.
+//
+
+#import "JSQChatApp.h"
+
+@implementation JSQChatApp
+
+@end
